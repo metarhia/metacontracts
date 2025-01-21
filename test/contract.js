322ef40ecafe52c5cb387381d6f4affbf1a3a01b
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2025-01-21 08:07:15 +0200

    Version 0.0.0

---
+++ test/contract.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const assert = require('node:assert');
+const test = require('node:test');
+const metacontracts = require('..');
+
+test('Stub', () => {
+  assert.ok(metacontracts);
+});
